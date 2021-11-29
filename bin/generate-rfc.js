0385033f7d251a565eefc12b809e17cf0e4e525e
Blaze <sandevabeykoon123@gmail.com>
2021-11-29 23:44:41 +0530

    update component composition with scss && project structure

--- bin/generate-rfc.js
+++ bin/generate-rfc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { generate } from '../index.js';
+import generate from '../index.js';
 
 const commandArgs = process.argv.slice(2);
 generate(commandArgs);
---
+++ src/commands.js
@@ -0,0 +1,70 @@
+import { readdirSync, mkdirSync, writeFileSync, readFileSync } from 'fs';
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+export const generate = args => {
+  const componentName = args
+    ?.find(arg => arg.includes('name='))
+    ?.split('=')[1];
+
+  if (!componentName) {
+    throw new Error(
+      'Component name must be provided: try name=myComponent',
+    );
+  } else if (componentName.length > 35) {
+    throw new Error('Component name must be 35 characters or less');
+  }
+
+  const componentDir = './src/components';
+  const existingDirectories = getDirectories(componentDir);
+  const newComponentDir = `${componentDir}/${componentName}`;
+
+  if (existingDirectories.includes(componentName)) {
+    throw new Error(
+      'This component name already exists. Please choose a different name.',
+    );
+  }
+
+  mkdirSync(newComponentDir);
+
+  const componentFileName =
+    componentName.charAt(0).toUpperCase() + componentName.slice(1);
+
+  writeFileSync(
+    `${newComponentDir}/${componentFileName}.module.scss`,
+    `.main {\n}\n`,
+  );
+
+  writeFileSync(
+    `${newComponentDir}/index.js`,
+    `export * from './${componentFileName}.jsx';\n`,
+  );
+
+  writeFileSync(
+    `${newComponentDir}/${componentFileName}.jsx`,
+    `import styles from './${componentFileName}.module.scss';
+
+export const ${componentFileName} = () => {  
+  return (
+    <div className={styles.main}>${componentName}</div>
+    )
+};`,
+  );
+
+  const strBuffer = readFileSync('./src/components/index.js');
+
+  const indexContents = strBuffer
+    .toString()
+    .split('\n')
+    .filter(each => each);
+
+  const updatedContents = [
+    ...indexContents,
+    `export * from './${componentName}';`,
+  ].sort((a, b) => a.length - b.length);
+
+  writeFileSync('./src/components/index.js', updatedContents.join('\n'));
+};
